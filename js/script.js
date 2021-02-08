257cef2f7d41aa4a576a8e87e4500186db48edad
Connor O'Brien <connor@hifranc.com>
2021-02-08 09:47:56 +0400

    Add answers

--- js/script.js
+++ js/script.js
@@ -1,44 +1,30 @@
+import axios from "axios";
+
 async function postRequest() {
-	const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-		method: "POST",
-		body: JSON.stringify({
-			title: "foo",
-			body: "bar",
-			userId: 1,
-		}),
-		headers: {
-			"Content-type": "application/json; charset=UTF-8",
-		},
+	const response = await axios.post("https://jsonplaceholder.typicode.com/posts", {
+		title: "foo",
+		body: "bar",
+		userId: 1,
 	});
-	const json = await response.json();
-	console.log(json);
+	console.log(response);
 }
 
 postRequest();
 
 async function putRequest() {
-	const response = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
-		method: "PUT",
-		body: JSON.stringify({
-			id: 1,
-			title: "foot",
-			body: "bart",
-			userId: 1,
-		}),
-		headers: {
-			"Content-type": "application/json; charset=UTF-8",
-		},
+	const response = await axios.put("https://jsonplaceholder.typicode.com/posts/1", {
+		id: 1,
+		title: "foot",
+		body: "bart",
+		userId: 1,
 	});
-	const json = await response.json();
-	console.log(json);
+	console.log(response);
 }
 
 putRequest();
 
 function deleteRequest() {
-	fetch("https://jsonplaceholder.typicode.com/posts/1", {
-		method: "DELETE",
-	});
+	axios.delete("https://jsonplaceholder.typicode.com/posts/1");
 }
 
 deleteRequest();
